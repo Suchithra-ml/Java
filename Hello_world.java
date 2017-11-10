4cd8e654db8e52d202bb402327beb21b55a52101
Suchithra-ml <31467190+Suchithra-ml@users.noreply.github.com>
2017-11-10 18:37:13 +0530

    Add files via upload

---
+++ Hello_world.java
@@ -0,0 +1,10 @@
+package helloWorld;
+
+public class Hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first web publication");
+	}
+
+}
